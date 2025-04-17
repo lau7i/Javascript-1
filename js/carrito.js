fe1d1f6ff71bba5e620929253b80408917f7c19c
Lautaro Iribarria <lauleoboca@gmail.com>
2025-04-17 03:31:21 -0300

    preparando entrega n2

---
+++ js/carrito.js
@@ -0,0 +1,18 @@
+const h1 = document.getElementById("titulo");
+h1.textContent = "Carrito";
+h1.style.backgroundColor = "yellow";
+h1.style.textAlign = "center";
+
+let cardStorage = localStorage.getItem("cardCamiseta");
+cardStorage = JSON.parse(cardStorage);
+
+let cardContainer = document.getElementById("card-section");
+function renderCamisetas(cardItems) {
+  cardItems.forEach((camiseta) => {
+    const card = document.createElement("div");
+    card.innerHTML = `<h3>${camiseta.año}</h3>
+                          <h4>${camiseta.precio}</h4>`;
+    cardContainer.appendChild(card);
+  });
+}
+renderCamisetas(cardStorage);
--- js/index.js
+++ js/index.js
@@ -1,160 +1,88 @@
-alert("Bienvenido al mundo boca");
+let titulo = document.getElementById("title");
+titulo.innerText = "Hola mundo Boca";
+titulo.style.backgroundColor = "yellow";
+titulo.style.textAlign = "center";
 
-function bienvenida() {
-  console.log("Hola mundo Boca");
-}
-bienvenida();
+let subtitulo = document.createElement("h2");
+subtitulo.innerHTML = "<span>Aguante Boca</span>";
+document.body.append(subtitulo);
+subtitulo.style.backgroundColor = "yellow";
+subtitulo.style.textAlign = "center";
 
-let nombreIngresado = prompt("Ingresar nombre");
-let apellidoIngresado = prompt("Ingresar apellido");
+let entrenadores = ["Bianchi", "Lorenzo", "Basile"];
+let dt = document.getElementById("dt");
 
-if (nombreIngresado != "" && apellidoIngresado != "") {
-  alert("Nombre: " + nombreIngresado + "\nApellido: " + apellidoIngresado);
-} else {
-  alert("Error: Ingresar nombre y apellido");
-}
-
-let entrada = prompt(
-  "Ingresa tu Equipo favorito de los 5 grandes o simplemente escribí otro"
-);
-switch (entrada) {
-  case "Boca":
-    alert("Este sitio es para vos");
-    break;
-  case "River":
-    alert("Este sitio no es para vos");
-    break;
-  case "Independiente":
-    alert("Este sitio no es para vos");
-    break;
-  case "Racing":
-    alert("Este sitio no es para vos");
-    break;
-  case "San lorenzo":
-    alert("Este sitio no es para vos");
-    break;
-  case "otro":
-    alert("Este sitio no es para vos");
-    break;
-  default:
-    alert("Vuelve a ingresar tu equipo");
+for (const entrenador of entrenadores) {
+  let li = document.createElement("li");
+  li.innerHTML = entrenador;
+  dt.appendChild(li);
 }
 
-let presupuesto = parseFloat(prompt("ingresa el presupusto de boca"));
-if (presupuesto >= 3500000) {
-  alert("Boca puede comprar a paredes");
-} else {
-  alert("Boca compra otro 5");
-}
+const h2 = document.getElementById("subtitulo");
+h2.textContent = "Entrenadores exitosos en la historia de boca";
+h2.style.backgroundColor = "lightblue";
+h2.style.textAlign = "center";
 
-let puntosboca = parseInt(prompt("Ingrese puntos de boca"));
-let puntostalleres = parseInt(prompt("ingrese puntos de talleres"));
-
-if (puntostalleres == puntosboca) {
-  alert("Habrá final de desempate entre Boca y Talleres");
-} else if (puntostalleres > puntosboca) {
-  alert("Talleres será el nuevo campeón del futbol argentino");
-} else if (puntostalleres < puntosboca) {
-  alert("Boca Juniors será el nuevo campeón del futbol argentino");
-} else {
-  alert("Otro equipo salio campeon");
-}
-
-function multiplicar() {
-  let numero1 = parseInt(
-    prompt("¿cuantos titulos crees que ganara boca en los proximos 10 años?")
-  );
-  let numero2 = 10;
-  let resultado = numero1 / numero2;
-  alert(`Boca ganará un promedio de ${resultado} titulos por año`);
-}
-multiplicar();
-
-const jugadoresdeBoca = [
-  "rojo",
-  "advincula",
-  "battaglia",
-  "blanco",
-  "delgado",
-  "alarcon",
-  "herrera",
-  "palacios",
-  "cavani",
+const camisetas = [
+  {
+    id: 1,
+    año: 2020,
+    precio: 100000,
+  },
+  {
+    id: 2,
+    año: 2021,
+    precio: 100000,
+  },
+  {
+    id: 3,
+    año: 2022,
+    precio: 110000,
+  },
+  {
+    id: 4,
+    año: 2023,
+    precio: 120000,
+  },
+  {
+    id: 5,
+    año: 2024,
+    precio: 130000,
+  },
+  {
+    id: 6,
+    año: 2025,
+    precio: 150000,
+  },
 ];
-jugadoresdeBoca.push("gimenez");
-jugadoresdeBoca.unshift("marchesín");
-jugadoresdeBoca.splice(6, 1, "zenón");
-console.log(jugadoresdeBoca);
-console.log(jugadoresdeBoca.includes("herrera"));
 
-const jugador12 = {
-  jugador: "Brey",
-  posicion: "arquero",
-  camiseta: 12,
-};
-const jugador13 = {
-  jugador: "Blondel",
-  posicion: "lateral por derecha",
-  camiseta: 42,
-};
-const jugador14 = {
-  jugador: "Di lollo",
-  posicion: "central por derecha",
-  camiseta: 40,
-};
-const jugador15 = {
-  jugador: "Saracchi",
-  posicion: "lateral por izquierda",
-  camiseta: 3,
-};
-const jugador16 = {
-  jugador: "belmonte",
-  posicion: "mediocampista",
-  camiseta: 30,
-};
-const jugador17 = {
-  jugador: "Zeballos",
-  posicion: "win",
-  camiseta: 7,
-};
-const jugador18 = {
-  jugador: "Merentiel",
-  posicion: "delantero",
-  camiseta: 16,
-};
+const cardCamiseta = [];
+let camisetasContainer = document.getElementById("camisetas-Container");
 
-const suplentes = {
-  jugador12,
-  jugador13,
-  jugador14,
-  jugador15,
-  jugador16,
-  jugador17,
-  jugador18,
-};
-console.log(suplentes);
-
-const bombonera = {
-  color: "azul y amarillo",
-  techo: false,
-  capacidad: 54000,
-  hinchada: "la12",
-  años: 85,
-  comida: "hamburguesa",
-};
-console.log(bombonera);
-
-let ingresaContraseña = prompt("ingrese contraseña");
-let pass = "boca123456";
-let ingresosIncorrectos = 0;
-while (ingresaContraseña != pass && ingresosIncorrectos < 2) {
-  alert("contrasña incorrecta");
-  ingresosIncorrectos++;
-  ingresaContraseña = prompt("ingresar contraseña");
+function renderCamisetas(camisetasArray) {
+  camisetasArray.forEach((camiseta) => {
+    const card = document.createElement("div");
+    card.innerHTML = `<h3>camiseta del año:${camiseta.año}</h3>
+                      <h4>$${camiseta.precio}</h4>
+                      <button class="Agregarproducto" id=${camiseta.id}>Agregar</button>`;
+    camisetasContainer.appendChild(card);
+  });
+  addToCardButton();
 }
+renderCamisetas(camisetas);
 
-if (ingresosIncorrectos == 2) {
-  alert("Acceso denegado");
-} else {
-  alert("Acceso concedido");
+function addToCardButton() {
+  addButton = document.querySelectorAll(".Agregarproducto");
+  addButton.forEach((button) => {
+    button.onclick = (e) => {
+      const camisetasId = e.currentTarget.id;
+      const selectedcamiseta = camisetas.find(
+        (camiseta) => camiseta.id == camisetasId
+      );
+      cardCamiseta.push(selectedcamiseta);
+      console.log(cardCamiseta);
+
+      localStorage.setItem("cardCamiseta", JSON.stringify(cardCamiseta));
+    };
+  });
 }
